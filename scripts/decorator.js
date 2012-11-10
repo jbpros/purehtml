3e1c99152c79128b544dc73ffee44b66a05bd7b3
Julien Biezemans <jb@jbpros.com>
2012-11-10 22:22:24 +0100

    Style

--- scripts/decorator.js
+++ scripts/decorator.js
@@ -1,7 +1,7 @@
 $(function () {
   $("article").wrap('<div class="container"><div class="row"><div class="span12"></div></div></div>');
 
-  $("section.introduction").wrap('<div class="text-success"/>');
+  $("section.introduction").wrap('<div class="lead"/>');
 
   // Twitter Bootstrap
   $("head").append('<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet"><script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js"></script>');
